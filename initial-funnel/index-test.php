17c9240423f57a5b050c35f5128b81648c4f98ce
Kîbs <90070256+robin-everaert@users.noreply.github.com>
2026-03-05 12:15:58 +0100

    commit

--- initial-funnel/index-test.php
+++ initial-funnel/index-test.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seitentitel</title>
+    <title>Page title</title>
 </head>
 <body>
-  <p>Reden ist Silber, Schweigen ist Gold</p>  
+  <p>Hi everyone</p>  
 </body>
 </html>
